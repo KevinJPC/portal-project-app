408591472204606d9f4d604c7ed32d42df10efe4
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-09-27 08:15:26 -0600

    Development of more routes and process pages

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../layout/Home'
+import UpdateUser from '../pages/user/UpdateUser'
+import UpdatePassword from '../pages/user/UpdatePassword'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import ActivesProcesses from '../pages/process/ActivesProcesses'
+import InactivesProcesses from '../pages/process/InactivesProcesses'
+import RegisterProcess from '../pages/process/RegisterProcess'
+import UpdateProcess from '../pages/process/UpdateProcess'
+import ActivesRoles from '../pages/role/ActivesRoles'
+import InactivesRoles from '../pages/role/InactivesRoles'
+import RegisterRole from '../pages/role/RegisterRole'
+import UpdateRole from '../pages/role/UpdateRole'
 
 const MainRoutes = () => {
 	return (
@@ -19,16 +29,58 @@ const MainRoutes = () => {
 					path='register'
 					element={<Register />}
 				/>
+				<Route path='dashboard'></Route>
+				<Route path='admin'>
+					<Route path='processes'>
+						<Route
+							index
+							path='actives'
+							element={<ActivesProcesses />}
+						/>
+						<Route
+							path='inactives'
+							element={<InactivesProcesses />}
+						/>
+						<Route
+							path='register'
+							element={<RegisterProcess />}
+						/>
+						<Route
+							path=':id'
+							element={<UpdateProcess />}
+						/>
+					</Route>
+					<Route path='roles'>
+						<Route
+							index
+							path='actives'
+							element={<ActivesRoles />}
+						/>
+						<Route
+							path='inactives'
+							element={<InactivesRoles />}
+						/>
+						<Route
+							path='register'
+							element={<RegisterRole />}
+						/>
+						<Route
+							path=':id'
+							element={<UpdateRole />}
+						/>
+					</Route>
+				</Route>
+				<Route path='profile'>
+					<Route
+						index
+						element={<UpdateUser />}
+					/>
+					<Route
+						path='update-password'
+						element={<UpdatePassword />}
+					/>
+				</Route>
 			</Route>
-			{/* <Route path="/admin">
-
-      </Route>
-      <Route path="/user">
-
-      </Route>
-      <Route path="/admin">
-
-      </Route> */}
 		</Routes>
 	)
 }
