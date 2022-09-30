acb977e9e72fe9395bfc8d93a29a175117b0b7c7
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2022-09-30 11:26:30 -0600

    Add Admins and Myprocceses to components

---
+++ src/components/Admins.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function Admins({
+	name,
+	percentageAdvance,
+	creationDate,
+	activitynow,
+	buttonText,
+	route,
+}) {
+	return (
+		<div className='flex flex-col pt-2 sm:pt-0 '>
+			<div className='flex justify-center py-4'>
+				<div className='w-3/4 bg-p-gray rounded'>
+					<div className='md:grid md:grid-cols-4 text-center gap-4 '>
+						<div className='p-6 px-1 break-words text-p-blue'>
+							<p className='text-ms  font-fira-medium font-medium leading-5'>
+								Nombre completo
+							</p>
+							<p className='text-center leading-normal pt-2'>{name}</p>
+						</div>
+						<div className='p-6 px-1 break-words text-p-blue'>
+							<p className='text-ms font-fira-medium font-medium leading-5'>
+								Correo electronico
+							</p>
+							<p className='text-center leading-normal pt-2'>{creationDate}</p>
+						</div>
+						<div className='p-6 px-1 break-words text-p-blue '>
+							<p className='text-ms font-medium leading-5 break-words font-fira-medium'>
+								Cédula
+							</p>
+							<p className='text-sm leading-normal pt-2'>{activitynow}</p>
+						</div>
+						<div className='p-6 px-1 break-words text-p-blue '>
+							<p className='text-ms font-medium leading-5 break-words font-fira-medium'>
+								Avance porcentual
+							</p>
+							<p className='text-sm leading-normal pt-2'>{percentageAdvance}</p>
+						</div>
+						<div className='p-8 text-center'>
+							<Link
+								to={route}
+								className='text-p-white bg-p-purple text-center font-medium rounded-lg text-xs sm:text-sm p-1.5 px-10 py-3 ml-4 md:mr-2'
+							>
+								{buttonText}
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+Admins.propTypes = {
+	name: PropTypes.string,
+	description: PropTypes.string,
+	activitynow: PropTypes.string,
+	percentageAdvance: PropTypes.string,
+	buttonText: PropTypes.string,
+	route: PropTypes.string,
+}
+export default Admins
