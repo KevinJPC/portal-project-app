b060275d197b9f3d8c94c7e72ba7a896b30fcca9
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2022-10-04 14:36:26 -0600

    Add about  processes

--- src/pages/process/AboutProcesses.js
+++ src/pages/process/AboutProcesses.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { ArrowDownIcon } from '@heroicons/react/24/solid'
 
 function AboutProcesses({
 	name,
@@ -49,11 +49,12 @@ function AboutProcesses({
 									Actividades
 								</p>
 							</div>
-							<div className='flex flex-row items-center gap-6'>
+
+							<div className='flex flex-row items-center gap-6 p-1'>
 								<p className='text-ms  font-fira-medium font-medium leading-5'>
-									Actividades
+									Actividad
 								</p>
-								<div className='p-4 shadow-sm bg-p-blue w-16 h-16 rounded-full'>
+								<div className='p-4 shadow-sm bg-p-silver w-16 h-16 rounded-full'>
 									1
 								</div>
 								<Link
@@ -63,8 +64,49 @@ function AboutProcesses({
 									Ir a actividad
 								</Link>
 							</div>
-							<div className='p-4 shadow-sm bg-p-blue w-16 h-16 rounded-full'>
-								1
+
+							<div className='flex flex-row items-center gap-6 p-1'>
+								<p className='text-ms  font-fira-medium font-medium leading-5'>
+									Actividad
+								</p>
+								<div className='p-4 shadow-sm bg-p-silver w-16 h-16 rounded-full'>
+									2
+								</div>
+								<Link
+									to='/'
+									className='text-p-white bg-p-purple text-center font-medium rounded-lg text-xs sm:text-sm p-1.5 px-10 py-3 ml-4 md:mr-2'
+								>
+									Ir a actividad
+								</Link>
+							</div>
+							<div className='flex flex-row items-center gap-6 p-1'>
+								<p className='text-ms  font-fira-medium font-medium leading-5'>
+									Actividad
+								</p>
+								<div className='p-4 shadow-sm bg-p-blue w-16 h-16 rounded-full'>
+									3
+								</div>
+								<Link
+									to='/'
+									className='text-p-white bg-p-purple text-center font-medium rounded-lg text-xs sm:text-sm p-1.5 px-10 py-3 ml-4 md:mr-2'
+								>
+									Ir a actividad
+								</Link>
+							</div>
+							<div></div>
+							<div className='flex flex-row items-center gap-6 p-1'>
+								<p className='text-ms  font-fira-medium font-medium leading-5'>
+									Actividad
+								</p>
+								<div className='p-4 shadow-sm bg-p-silver w-16 h-16 rounded-full'>
+									4
+								</div>
+								<Link
+									to='/'
+									className='text-p-white bg-p-purple text-center font-medium rounded-lg text-xs sm:text-sm p-1.5 px-10 py-3 ml-4 md:mr-2'
+								>
+									Ir a actividad
+								</Link>
 							</div>
 						</div>
 					</div>
