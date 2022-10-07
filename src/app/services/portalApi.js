749cd6094fe71f2a55d70b5e859ff713064af76a
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-10-07 15:40:21 -0600

    Testing

--- src/app/services/portalApi.js
+++ src/app/services/portalApi.js
@@ -4,17 +4,15 @@ export const portalApi = createApi({
 	reducerPath: 'portalApi',
 	refetchOnFocus: true,
 	refetchOnReconnect: true,
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://127.0.0.1:8000/api/v1/',
-		prepareHeaders: (headers, { getState }) => {
-			const token =
-				'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NzU0M2MzNi1kMDVmLTQ3N2YtODVmZi0yM2RlMGI2OGYxMTciLCJqdGkiOiI2MDJlMDhiMTIxNGU2NTY5YmQwNjUyMWQ5N2JiZjY1ODFjYmI1ZjI3ZDgzNGNmZGRkNjkxY2Y5ZDQ3MjYwYzIyY2QwMmRhNjA4NWI0OTFmYyIsImlhdCI6MTY2NTE3NDg2OS40NDUwMzUsIm5iZiI6MTY2NTE3NDg2OS40NDUwMzcsImV4cCI6MTY2NTI2MTI2OS40MzExNzcsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.u-eT-oxpgJdaG9YsnaY7c6IGwu3wln50ssA9tMUpYDcr01gaWG4pnr1f7gDVFc6RgsW-8rRTAhvfCQ7n0p9ic4uGexBo3Q7K7njP7_xYwTR_vjRbFLu_5xAbCItq50nWQ8lc3IyvGUMTeNBuJfL3_fkhlxySeAKXkCXZ_jeFqq0bhaSC5kf9IyP248WTxFAGnGvWrlAF9Ss8F_v9d-LwI5a9frB5eigWe4eJABe04w_dborl_A9PKNvfhnX2gstHQWLX76JnysTEvE1SREXC8U_aWnmvPLO0az0dHJM02MHZZA2NPLhat-8AYMDFEz5o9PAJK4Qwv1-CVX_D1u__If25JxBwE08wJiL1fF3b8g_XeIceC40HkOXRqTX-VEjcmgW3OL7krYz9pjJV2r0S6MpqlknYyBZOjit_TK7ts0xM6HBMDKK4Q0D61-J-0r915da2EDOJP5wnenxdddDb6DU2maREBycJnz3pyCirtFILpkKODKUO82VTOEuXambJ-zY5TwLdlYtd38Wuq74U36l1H89NBOYSHrQnByivJ8fgED_HQaCyHp5FSY0VFY8oiR7qSP096ZghQ53iQBa7PTwmZ9vQtYjW2SAYg2kujGKPuP7BeehHk6zGX10KaQ98R41wuvI6OTWGZvnaIB9GGd-N28sxplx-C6c-8gNclvw'
-			headers.set('authorization', `Bearer ${token}`)
-			headers.set('Accept', 'application/json')
-			headers.set('Content-Type', 'application/json')
-			return headers
-		},
-	}),
+	baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/v1/' }),
+	prepareHeaders: (headers, { getState }) => {
+		const token =
+			'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NzRmY2MyYy01YzBlLTQ1MTEtOGZkMC00MGE5M2I4ODZhMWUiLCJqdGkiOiI0ZTcwZjNiOTk4OGU5ZWJhOWJhNmZjNTM2ZGUzNjc3YzlhZTU4M2JjZDI2ZDZiNzNlYWI1MzgyMWY3MmVmMjIzODgxZjk2NGEwNGRjZWZkMSIsImlhdCI6MTY2NTA4NTMxMC44NDc5MiwibmJmIjoxNjY1MDg1MzEwLjg0NzkyMiwiZXhwIjoxNjY1MTcxNzEwLjc3Mjg1NCwic3ViIjoiMiIsInNjb3BlcyI6W119.c5kEjKJXaZ6uCFETRGU5byggWP0N4ot37qxW_eq4rxJGpOIMcBfCXi2m6k59buA6j91VwaNQpmYT1ipmKktcA3uMhq1ON7_q2LgTUczB9flFj7KBFHTpT5gmJUQee6GD4uzIG10l3wPweP3rk3ZEmVL_RAJbMT-GAXTeDqv3riYunuzZl6H0pBztaEp0x9oR9vCtnO1_tK9K5tF9-QUquxFpP7rM0u-QxmZTityNaw2yEvmm8_YGeeJIDZydcBACMuZSgkOJvvre8sO7tmtDO4nbSmqLWdCCPeRuwud2afVzgT6bffQeqTViGRhdfKD13G6behJj0KbaD94864pwh7Zk-0gdqJUQDf1ze8MZqOawM5FOzkNVcC7XYCXxZcESBIXxKEGjl9fK61aXPissUKIXecSXSX4AnlVS2COUw01lpW99eGrVS5Eq71rUi4mDAUAB_YsBm2yrIRNfIfxYWqFtsupXvpTJcofGIUeLwYgSaa8FyyFrx0G1lKlflVPH7CvQwAmXZpBwGrgAMc8TlK20jMEoHOLVUUfvZA_W1kJ9mhqBectUhN8WePlkeGzOOoANQVZvn4aGOXGcjmDr7oqxi9Jt0x_izkpvjM8akQvcfqx1kxFw3GMYUn6l3s8xm8e2Umu_1A5j_y3jPFJZI8Ls9uCVt1uKKSIjimJ0KFA'
+		headers.set('authorization', `Bearer ${token}`)
+		headers.set('Accept', 'application/json')
+		headers.set('Content-Type', 'application/json')
+		return headers
+	},
 	tagTypes: ['User', 'Process', 'Role', 'Admin', 'Insider'],
 	endpoints: builder => ({}),
 })
