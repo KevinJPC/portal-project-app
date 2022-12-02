4206a058e92edd821c22f670d84ed74b7601af8a
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-12-02 14:52:09 -0600

    Adding formik and yup validation to admin pages

--- src/pages/admin/RegisterAdmin.js
+++ src/pages/admin/RegisterAdmin.js
@@ -1,117 +1,99 @@
 import React from 'react'
-import Input from '../../components/inputs/TextInput'
-import PasswordInput from '../../components/inputs/PasswordInput'
+import { Form, Formik } from 'formik'
 import SubmitButton from '../../components/buttons/SubmitButton'
-import Alert from '../../components/alerts/Alert'
-import useForm from '../../hooks/useForm'
 import useAdmin from '../../hooks/useAdmin'
+import { adminSchema } from '../../formYupSchemas/adminSchema'
+import InputText from '../../components/inputs/InputText'
+import InputPassword from '../../components/inputs/InputPassword'
 
 const RegisterAdmin = () => {
 	const {
-		formState,
-		dni,
-		name,
-		email,
-		firstLastName,
-		secondLastName,
-		password,
-		passwordConfirmation,
-		onInputChange,
-	} = useForm({
-		dni: '',
-		name: '',
-		email: '',
-		firstLastName: '',
-		secondLastName: '',
-		password: '',
-		passwordConfirmation: '',
-	})
-
-	const {
 		registerProps: { registerNewAdmin, isLoadingAddNewAdmin },
-	} = useAdmin({ ...formState })
+	} = useAdmin()
 
 	return (
 		<div className=''>
 			<div className='flex flex-col items-center pt-10 md:pt-14 mt-14'>
-				<div>
-					<h3 className='text-3xl text-p-blue'>Registrar administrador</h3>
-				</div>
-				<div className='w-full py-4 mt-1 px-4 overflow-hidde max-w-xs md:max-w-3xl'>
-					<form onSubmit={registerNewAdmin}>
-						<div className='md:grid md:grid-cols-2 gap-5 '>
-							<div className='mt-4 '>
-								<Input
-									id='dni'
-									label='Cédula'
-									placeholder='Cédula'
-									value={dni}
-									onChange={onInputChange}
-								/>
+				<Formik
+					initialValues={{
+						dni: '',
+						name: '',
+						email: '',
+						firstLastName: '',
+						secondLastName: '',
+						password: '',
+						passwordConfirmation: '',
+					}}
+					onSubmit={(values, { resetForm }) => {
+						registerNewAdmin({ ...values })
+						resetForm()
+					}}
+					validationSchema={adminSchema}
+				>
+					{() => (
+						<>
+							<div>
+								<h3 className='text-3xl text-p-blue'>
+									Registrar administrador
+								</h3>
 							</div>
-							<div className='mt-4 '>
-								<Input
-									id='name'
-									label='Nombre'
-									placeholder='Nombre'
-									value={name}
-									onChange={onInputChange}
-								/>
+							<div className='w-full py-4 mt-1 px-4 overflow-hidde max-w-xs md:max-w-3xl'>
+								<Form>
+									<div className='md:grid md:grid-cols-2 gap-5 '>
+										<div className='mt-4 '>
+											<InputText
+												label='Cédula'
+												name='dni'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Nombre'
+												name='name'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Primer apellido'
+												name='firstLastName'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Segundo apellido'
+												name='secondLastName'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Correo electrónico'
+												name='email'
+											/>
+										</div>
+										<div className='mt-4'>
+											<InputPassword
+												label='Contraseña'
+												name='password'
+											/>
+										</div>
+										<div className='mt-4 mb-4'>
+											<InputPassword
+												name='passwordConfirmation'
+												label='Confirmar contraseña'
+											/>
+										</div>
+									</div>
+									<div className='md:px-36 my-5'>
+										<SubmitButton
+											isLoading={isLoadingAddNewAdmin}
+											text='Registrar'
+										/>
+									</div>
+								</Form>
 							</div>
-							<div className='mt-4 '>
-								<Input
-									id='firstLastName'
-									label='Primer apellido'
-									placeholder='Primer apellido'
-									value={firstLastName}
-									onChange={onInputChange}
-								/>
-							</div>
-							<div className='mt-4 '>
-								<Input
-									id='secondLastName'
-									label='Segundo apellido'
-									placeholder='Segundo apellido'
-									value={secondLastName}
-									onChange={onInputChange}
-								/>
-							</div>
-							<div className='mt-4 '>
-								<Input
-									id='email'
-									label='Correo electrónico'
-									placeholder='Correo electrónico'
-									value={email}
-									onChange={onInputChange}
-								/>
-							</div>
-							<div className='mt-4'>
-								<PasswordInput
-									id='password'
-									label='Contraseña'
-									placeholder='Contraseña'
-									value={password}
-									onChange={onInputChange}
-								/>
-							</div>
-							<div className='mt-4 mb-4'>
-								<PasswordInput
-									id='passwordConfirmation'
-									label='Confirmar Contraseña'
-									placeholder='Confirmar Contraseña'
-									value={passwordConfirmation}
-									onChange={onInputChange}
-								/>
-							</div>
-						</div>
-						<div className='md:px-36 my-5'>
-							<SubmitButton
-								isLoading={isLoadingAddNewAdmin}
-								text='Registrar'
-							/>
-						</div>
-					</form>
-				</div>
+						</>
+					)}
+				</Formik>
 			</div>
 		</div>
 	)
--- src/pages/admin/UpdateAdmin.js
+++ src/pages/admin/UpdateAdmin.js
@@ -1,34 +1,25 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import Alert from '../../components/alerts/Alert'
+import { Form, Formik } from 'formik'
 import ClickButton from '../../components/buttons/ClickButton'
 import SubmitButton from '../../components/buttons/SubmitButton'
-import Input from '../../components/inputs/TextInput'
 import ModalWindow from '../../components/ModalWindow'
 import useAdmin from '../../hooks/useAdmin'
 import useForm from '../../hooks/useForm'
+import InputText from '../../components/inputs/InputText'
+import { adminSchema } from '../../formYupSchemas/adminSchema'
 
 const UpdateAdmin = () => {
 	const { id } = useParams()
 
-	const {
-		formState,
-		name,
-		email,
-		firstLastName,
-		secondLastName,
-		showModal,
-		onInputChange,
-		changeFormState,
-		closeModal,
-		openModal,
-	} = useForm({
-		id,
-		name: '',
-		email: '',
-		firstLastName: '',
-		secondLastName: '',
-	})
+	const { formState, showModal, changeFormState, closeModal, openModal } =
+		useForm({
+			id,
+			name: '',
+			email: '',
+			firstLastName: '',
+			secondLastName: '',
+		})
 
 	const {
 		updateProps: {
@@ -40,10 +31,10 @@ const UpdateAdmin = () => {
 		getUserAdminInformation,
 		isSuccessGetAdmin,
 		userInformation,
-	} = useAdmin({ ...formState }, id)
+	} = useAdmin()
 
 	useEffect(() => {
-		getUserAdminInformation()
+		getUserAdminInformation(id)
 	}, [userInformation])
 
 	useEffect(() => {
@@ -60,73 +51,76 @@ const UpdateAdmin = () => {
 	return (
 		<div className=''>
 			<div className='flex flex-col items-center pt-10 md:pt-2 mt-14'>
-				<div>
-					<h3 className='text-3xl text-p-blue'>Modificar usuario</h3>
-				</div>
-				<div className='w-full py-4 mt-1 px-4 overflow-hidde max-w-xs md:max-w-3xl'>
-					<form onSubmit={updateAdminUser}>
-						<div className='md:grid md:grid-cols-2 gap-5 '>
-							<div className='mt-4 '>
-								<Input
-									id='name'
-									label='Nombre'
-									placeholder='Nombre'
-									value={name}
-									onChange={onInputChange}
-								/>
+				<Formik
+					initialValues={{
+						...formState,
+					}}
+					onSubmit={values => {
+						updateAdminUser({ id, ...values })
+					}}
+					enableReinitialize
+					validationSchema={adminSchema}
+				>
+					{() => (
+						<>
+							<div>
+								<h3 className='text-3xl text-p-blue'>Modificar usuario</h3>
 							</div>
-							<div className='mt-4 '>
-								<Input
-									id='firstLastName'
-									label='Primer apellido'
-									placeholder='Primer apellido'
-									value={firstLastName}
-									onChange={onInputChange}
-								/>
+							<div className='w-full py-4 mt-1 px-4 overflow-hidde max-w-xs md:max-w-3xl'>
+								<Form>
+									<div className='md:grid md:grid-cols-2 gap-5 '>
+										<div className='mt-4 '>
+											<InputText
+												label='Nombre'
+												name='name'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Primer apellido'
+												name='firstLastName'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Segundo apellido'
+												name='secondLastName'
+											/>
+										</div>
+										<div className='mt-4 '>
+											<InputText
+												label='Correo electrónico'
+												name='email'
+											/>
+										</div>
+									</div>
+									<div className='md:px-36'>
+										<ClickButton
+											isLoading={isLoadingInactivateAdmin}
+											text='Desactivar'
+											func={openModal}
+											color='red'
+										/>
+										<div className='mt-3'>
+											<SubmitButton
+												isLoading={isLoadingUpdateAdmin}
+												text='Guardar cambios'
+											/>
+										</div>
+									</div>
+								</Form>
 							</div>
-							<div className='mt-4 '>
-								<Input
-									id='secondLastName'
-									label='Segundo apellido'
-									placeholder='Segundo apellido'
-									value={secondLastName}
-									onChange={onInputChange}
-								/>
-							</div>
-							<div className='mt-4 '>
-								<Input
-									id='email'
-									label='Correo electrónico'
-									placeholder='Correo electrónico'
-									value={email}
-									onChange={onInputChange}
-								/>
-							</div>
-						</div>
-						<div className='md:px-36'>
-							<ClickButton
-								isLoading={isLoadingInactivateAdmin}
-								text='Desactivar'
-								func={openModal}
-								color='red'
-							/>
-							{showModal && (
-								<ModalWindow
-									text='¿Está seguro de inactivar este registro?'
-									buttonText='Desactivar'
-									setShowModal={closeModal}
-									onDialog={inactivaUserAdmin}
-								/>
-							)}
-							<div className='mt-3'>
-								<SubmitButton
-									isLoading={isLoadingUpdateAdmin}
-									text='Guardar cambios'
-								/>
-							</div>
-						</div>
-					</form>
-				</div>
+						</>
+					)}
+				</Formik>
+				{showModal && (
+					<ModalWindow
+						text='¿Está seguro de inactivar este registro?'
+						buttonText='Desactivar'
+						setShowModal={closeModal}
+						onDialog={() => inactivaUserAdmin(id)}
+					/>
+				)}
 			</div>
 		</div>
 	)
